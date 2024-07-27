33585b90bad7021ba9c64064eedc7210092f1854
abhilashpanda04 <abhilashk.isme1517@gmail.com>
2024-07-28 00:01:20 +0530

    cleaning data step completed

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,7 +1,37 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.data_cleaning import DataCleaning,DataDevideStretegy,DataPreProcessingStrategy
+from typing_extensions import Annotated
+from typing import Tuple
 
 @step
-def clean_data(df:pd.DataFrame)->None:
-    pass
+def clean_data(df:pd.DataFrame)->Tuple[
+    Annotated[pd.DataFrame, "x_train"],
+    Annotated[pd.DataFrame, "x_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+    ]:
+    """
+    cleans the data and devides it into train and test
+    Args:
+        df: pd.DataFrame
+    Returns:
+        x_train: Training Data
+        x_test: Testing Data
+        y_train: Training Labels
+        y_test: Testing Labels
+    """
+    try:
+        preprocess_stretegy=DataPreProcessingStrategy()
+        datacleaning=DataCleaning(df,preprocess_stretegy)
+        processed_data=datacleaning.handle_data()
+
+
+        devide_stretegy=DataDevideStretegy()
+        datacleaning=DataCleaning(processed_data,devide_stretegy)
+        x_train,x_test,y_train,y_test=datacleaning.handle_data()
+        logging.info("Datacleaning completed")
+    except Exception as e:
+        logging.error(f"Error in cleaning data: {e}")
+        raise e
