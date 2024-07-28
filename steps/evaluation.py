2e9f4267825f2e8b544f7462e95564f3d994d54e
abhilashpanda04 <abhilashk.isme1517@gmail.com>
2024-07-28 10:38:42 +0530

    evaluation step completed

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,11 +1,36 @@
 import logging
 from zenml import step
 import pandas as pd
+from sklearn.base import RegressorMixin
+from src.evaluation import MSE,R2,RMSE
+from typing_extensions import Annotated
+from typing import Tuple
+
 @step
-def evaluate_model(df)->None:
+def evaluate_model(model:RegressorMixin,
+                   x_test:pd.DataFrame,
+                   y_test:pd.DataFrame)->Tuple[Annotated[float,"r2_score"],
+                                               Annotated[float,"rmse"]]:
     """
     Evaluates Model on ingested data
     Args:
-        df: the ingested Data
+        model: Trained model
     """
-    pass
+    try:
+        y_pred=model.predict(x_test)
+        mse=MSE()
+        mse_score=mse.calculate_score(y_test,y_pred)
+
+        r2=R2()
+        r2_scorer=r2.calculate_score(y_test,y_pred)
+
+        rmse=RMSE()
+        rmse_score=rmse.calculate_score(y_test,y_pred)
+
+        return r2_scorer,rmse_score
+    except Exception as e:
+        logging.error(f"Error occured while evaluating model {e}")
+        raise e
+
+
+    
