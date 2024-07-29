c8b27640a883691542b08aa94668c1d211bae192
abhilashpanda04 <abhilashk.isme1517@gmail.com>
2024-07-29 19:53:47 +0530

    updated with config

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,8 +1,9 @@
 from pipeline.training_pipeline import train_pipeline
 
+
 if __name__ == "__main__":
-    data_path=r"/Users/abhilashkumarpanda/Desktop/devlopement/mlops/data/olist_customers_dataset.csv"
-    train_pipeline(data_path)
+    # data_path=r"/Users/abhilashkumarpanda/Desktop/devlopement/mlops/data/olist_customers_dataset.csv"
+    train_pipeline.with_options(config_path="configuration/config.yaml")()
 
 
 
